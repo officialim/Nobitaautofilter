926597dee8534fd77d807c514a128c0eb9cb148c
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-11-30 18:46:59 +0530

    Update pm_filter.py

--- LUCIFER/pm_filter.py
+++ LUCIFER/pm_filter.py
@@ -1221,6 +1221,7 @@ async def advantage_spell_chok(msg):
     )
     await asyncio.sleep(SPL_DELETE_TIME)
     await spell_check_del.delete()
+    await msg.delete()
     return
 
 async def manual_filters(client, message, text=False):
