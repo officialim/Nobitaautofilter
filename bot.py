3a6fc2d1f4f5c01c2fc369e58b821b8630036893
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-04 20:27:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import math
 import logging
 import logging.config
 from aiohttp import web
-from plugins import web_server
+from LUCIFER import web_server
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
