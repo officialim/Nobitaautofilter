cc0e4ed7ea6cefce84b85be0f3eb16e5ab86833c
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-22 08:53:51 +0530

    Update p_ttishow.py

--- LUCIFER/p_ttishow.py
+++ LUCIFER/p_ttishow.py
@@ -274,27 +274,12 @@ async def list_users(bot, message):
         out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
         if user['ban_status']['is_banned']:
             out += '( Banned User )'
-        out += '\n'
-    try:
-        await raju.edit_text(out)
-    except MessageTooLong:
-        with open('users.txt', 'w+') as outfile:
-            outfile.write(out)
-        await message.reply_document('users.txt', caption="List Of Users")
+        out += '\n'@Client.on_message(filters.command('invite') & filters.user(ADMINS))async def gen_invite(bot, message): if len(message.command) == 1: return await message.reply('Give me a chat id') chat = message.command[1] try: chat = int(chat) except: return await message.reply('Give Me A Valid Chat ID') try: link = await bot.create_chat_invite_link(chat) except ChatAdminRequired: return await message.reply("Invite Link Generation Failed, Iam Not Having Sufficient Rights") except Exception as e: return await message.reply(f'Error {e}') await message.reply(f'Here is your Invite Link {link.invite_link}')
+@Client.on_message(filters.command('ban_user') & filters.user(ADMINS))async def ban_a_user(bot, message): # https://t.me/GetTGLink/4185 if len(message.command) == 1: return await message.reply('Give me a user id / username') r = message.text.split(None) if len(r) > 2: reason = message.text.split(None, 2)[2] chat = message.text.split(None, 2)[1] else: chat = message.command[1] reason = "No reason Provided" try: chat = int(chat) except: pass try: k = await bot.get_users(chat) except PeerIdInvalid: return await message.reply("This is an invalid user, make sure ia have met him before.") except IndexError: return await message.reply("This might be a channel, make sure its a user.") except Exception as e: return await message.reply(f'Error - {e}') else: jar = await db.get_ban_status(k.id) if jar['is_banned']: return await message.reply(f"{k.mention} is already banned\nReason: {jar['ban_reason']}") await db.ban_user(k.id, reason) temp.BANNED_USERS.append(k.id) await message.reply(f"Successfully banned {k.mention}") await bot.send_message(LOG_CHANNEL, script.BANP_LOG_TXT.format(message.from_user.mention, k.mention, reason))
 
-@Client.on_message(filters.command('chats') & filters.user(ADMINS))
-async def list_chats(bot, message):
-    raju = await message.reply('Getting List Of chats')
-    chats = await db.get_all_chats()
-    out = "Chats Saved In DB Are:\n\n"
-    async for chat in chats:
-        out += f"**Title:** `{chat['title']}`\n**- ID:** `{chat['id']}`"
-        if chat['chat_status']['is_disabled']:
-            out += '( Disabled Chat )'
-        out += '\n'
-    try:
-        await raju.edit_text(out)
-    except MessageTooLong:
-        with open('chats.txt', 'w+') as outfile:
-            outfile.write(out)
-        await message.reply_document('chats.txt', caption="List Of Chats")
+
+@Client.on_message(filters.command('unban_user') & filters.user(ADMINS))async def unban_a_user(bot, message): if len(message.command) == 1: return await message.reply('Give me a user id / username') r = message.text.split(None) if len(r) > 2: reason = message.text.split(None, 2)[2] chat = message.text.split(None, 2)[1] else: chat = message.command[1] reason = "No reason Provided" try: chat = int(chat) except: pass try: k = await bot.get_users(chat) except PeerIdInvalid: return await message.reply("This is an invalid user, make sure ia have met him before.") except IndexError: return await message.reply("This might be a channel, make sure its a user.") except Exception as e: return await message.reply(f'Error - {e}') else: jar = await db.get_ban_status(k.id) if not jar['is_banned']: return await message.reply(f"{k.mention} is not yet banned.") await db.remove_ban(k.id) temp.BANNED_USERS.remove(k.id) await message.reply(f"Successfully unbanned {k.mention}") await bot.send_message(LOG_CHANNEL, script.UNBANP_LOG_TXT.format(message.from_user.mention, k.mention))
+
+
+@Client.on_message(filters.command('users') & filters.user(ADMINS))async def list_users(bot, message): # https://t.me/GetTGLink/4184 raju = await message.reply('Getting List Of Users') users = await db.get_all_users() out = "Users Saved In DB Are:\n\n" async for user in users: out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>" if user['ban_status']['is_banned']: out += '( Banned User )' out += '\n' try: await raju.edit_text(out) except MessageTooLong: with open('users.txt', 'w+') as outfile: outfile.write(out) await message.reply_document('users.txt', caption="List Of Users")
+@Client.on_message(filters.command('chats') & filters.user(ADMINS))async def list_chats(bot, message): raju = await message.reply('Getting List Of chats') chats = await db.get_all_chats() out = "Chats Saved In DB Are:\n\n" async for chat in chats: out += f"Title: {chat['title']}\n- ID: {chat['id']}" if chat['chat_status']['is_disabled']: out += '( Disabled Chat )' out += '\n' try: await raju.edit_text(out) except MessageTooLong: with open('chats.txt', 'w+') as outfile: outfile.write(out) await message.reply_document('chats.txt', caption="List Of Chats")
